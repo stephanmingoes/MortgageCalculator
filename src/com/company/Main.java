3cca193f8fa1bcfcc4c4325d471fd83d017ce23b
Stephan Mingoes <stephsvpreme@gmail.com>
2022-01-12 22:51:10 -0500

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,27 +7,50 @@ public class Main {
 
     public static void main(String[] args) {
 
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
         Scanner scanner = new Scanner(System.in);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal;
+        float AnnualRate;
+        int NumberOfYears;
+        float MonthlyRate;
+        int NumberOfPayments;
 
-        System.out.print("Annual Interest Rate: ");
-        float annual_rate = scanner.nextFloat();
-        float monthly_rate = annual_rate / PERCENT / MONTHS_IN_YEAR;
 
-        System.out.print("Period (Years): ");
-        byte number_of_years = scanner.nextByte();
-        int number_of_payments = number_of_years * MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1000000)
+                break;
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+        }
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            AnnualRate = scanner.nextFloat();
+            if (AnnualRate > 0 && AnnualRate <= 30){
+                MonthlyRate = AnnualRate/ PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value greater than 0 or less than or equal to 30.");
+        }
+        while (true) {
+            System.out.print("Period (Years): ");
+            NumberOfYears = scanner.nextInt();
+            if (NumberOfYears > 0 && NumberOfYears <=30) {
+                NumberOfPayments = NumberOfYears * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30.");
+        }
 
-        double mortgage = principal * (monthly_rate * (Math.pow((1 + monthly_rate), number_of_payments)) / (Math.pow((1+monthly_rate), number_of_payments) - 1));
+        double mortgage = principal * (MonthlyRate * (Math.pow((1 + MonthlyRate), NumberOfPayments)) / (Math.pow((1+MonthlyRate), NumberOfPayments) - 1));
 
-//        mortgage = (float) Math.round(mortgage * 100) / 100;
-        String result = currency.format(mortgage);
+        System.out.println("Mortgage = " + currency.format(mortgage) );
 
-        System.out.println("Mortgage: "+ result);
+
+
     }
 }
